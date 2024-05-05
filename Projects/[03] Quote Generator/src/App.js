830c1281122e5fb5bad108eb5877228fb01963fe
Thvithran <73452153+thvithran@users.noreply.github.com>
2024-05-06 02:44:04 +0800

    Add files via upload

---
+++ Projects/[03] Quote Generator/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quotes from "./components/Quotes"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Quotes />
+    </div>
+  );
+}
+
+
